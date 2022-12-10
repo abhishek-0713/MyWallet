2c20cfb1f19a59735991821cbd78a3b98e679f62
Coder Abhi <coder.abhi0713@gmail.com>
2022-12-10 16:21:57 +0530

    Initial Commit

---
+++ Payment-wallet-Application/src/main/java/com/masai/PaymentWalletApplication.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentWalletApplication.class, args);
+	}
+
+}
