e86c6be430b418f87a881be9f4b98d115c5d45c6
GandhamRaghavendra <graghavendramutry13@gmail.com>
2022-12-30 17:41:43 +0530

    day3

--- MyWallet/src/main/java/com/mywallet/repository/WalletRepo.java
+++ MyWallet/src/main/java/com/mywallet/repository/WalletRepo.java
@@ -2,11 +2,14 @@ package com.mywallet.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.mywallet.model.Wallet;
 
 @Repository
 public interface WalletRepo extends JpaRepository<Wallet, Integer> {
-		 
+
+    @Query("FROM Wallet w INNER JOIN w.customer c WHERE c.customerId=?1")
+    public Wallet showCustomerWalletDetails(Integer customerId);
 }
